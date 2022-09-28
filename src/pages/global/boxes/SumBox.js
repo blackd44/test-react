325e2d8bc35ffd922bdb829679d66703c317b92e
blackd44 <irabd44@gmail.com>
2022-09-28 17:03:11 +0200

    structuring the project

---
+++ src/pages/global/boxes/SumBox.js
@@ -0,0 +1,21 @@
+
+
+const SumBox = (props) => {
+    return(
+        <div className='summary'>
+            <div className='sum-num'>{props.body.num}</div>
+            <div className='sum-img'>
+                <img alt='img' src={props.image}/>
+            </div>
+            <div className='sum-topic'>
+                <p><b>{props.body.title}</b></p>
+                <p>
+                    {props.body.content}<br/>
+                    {props.body.until}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default SumBox
