041d9183bab3c30bcb5ac3d0f84901f31b8b4d51
blackd44 <irabd44@gmail.com>
2022-10-02 20:09:21 +0200

    changing hashRoute to brouseRoute

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './setup/reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <BrowserRouter>
+      <React.Fragment>
+        <App />
+      </React.Fragment>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
